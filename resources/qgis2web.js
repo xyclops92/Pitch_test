9a33fc76aaf9f578bd6cc9155cc505629ff67aab
MapsBybebzy <73559783+xyclops92@users.noreply.github.com>
2025-01-04 10:21:33 +0000

    Update qgis2web.js

--- resources/qgis2web.js
+++ resources/qgis2web.js
@@ -937,9 +937,7 @@ var bottomAttribution = new ol.control.Attribution({
 map.addControl(bottomAttribution);
 
 var attributionList = document.createElement('li');
-attributionList.innerHTML = `
-	<a href="https://github.com/tomchadwin/qgis2web">qgis2web</a> &middot;
-	<a href="https://openlayers.org/">OpenLayers</a> &middot;
+attributionList.innerHTML = 
 	<a href="https://qgis.org/">QGIS</a>	
 `;
 bottomAttribution.element.appendChild(attributionList);
